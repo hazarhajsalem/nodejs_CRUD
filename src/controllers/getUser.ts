a70d01387d457ebc0c54631e3ab90be9e3127445
hazarhajsalem <hazarhaj2000@gmail.com>
2022-08-16 10:29:37 +0100

    fix: fix 'getUser' component content

--- src/controllers/getUser.ts
+++ src/controllers/getUser.ts
@@ -2,10 +2,26 @@ import pool from "../database";
 import { Request, Response } from "express";
 
 export const getUser = async (req: Request, res: Response) => {
-
+  const email = req.body.email
+  const psw = req.body.password
   try {
-    const result = await pool.query("SELECT * FROM test.saber");
-    res.status(200).json(result);
+    const result = await pool.query(`SELECT * FROM public.User Where email='${email}'`);
+
+    if(result.rows.length > 0)
+    { 
+        if(result.rows[0].psw === psw)
+        {
+          res.json({"errors": true, "data":"Email exist "})
+        }
+        else
+        {
+          res.json({"errors": false, "data":"password uncorrect !! "})
+        }
+    }
+    else
+    {
+      res.json({"errors": false, "data":"Email doesn't exist "})
+    }
   } catch (error) {
     console.log(error);
     return res.status(500).json({ error: error });
