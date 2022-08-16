3b8b9f41f12f456bb9ff3f667ed1cb86f41d9376
hazarhajsalem <hazarhaj2000@gmail.com>
2022-08-16 10:23:53 +0100

    fix: fix 'CommandeRouter' component content

--- src/router/CommandeRouter.ts
+++ src/router/CommandeRouter.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { addLivre } from "../controllers/addLivre";
 import { postUser } from "../controllers/postUser";
 import { getUser } from "../controllers/getUser";
 import { deleteUser } from "../controllers/deleteUser";
@@ -7,17 +8,24 @@ const router = express.Router();
 
 /**
  * @swagger
- * tags:
- *   name: User
- *   description: The books managing API
- */
-
-/**
- * @swagger
- * /commands:
- *   get:
- *     summary: get User
- *     tags: [User]
+ * /add:
+ *   post:
+ *     summary: add Livre
+ *     tags: [Livre]
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               isbn:
+ *                 type: string
+ *               titre:
+ *                 type: string
+ *               auteur:
+ *                 type: string
+ *               categorie:
+ *                 type: string
  *     responses:
  *       200:
  *         description: ok
@@ -36,15 +44,15 @@ const router = express.Router();
  *         content:
  *           application/json:
  *             schema:
- *                type: object
- *
+ *                type: object 
  */
+ router.route("/add").post(addLivre);
 
 /**
  * @swagger
- * /delete:
- *   delete:
- *     summary: get User
+ * /addUser:
+ *   post:
+ *     summary: post User
  *     tags: [User]
  *     requestBody:
  *       content:
@@ -52,7 +60,47 @@ const router = express.Router();
  *           schema:
  *             type: object
  *             properties:
- *               nom:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: ok
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *       404:
+ *         description: not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *       500:
+ *         description: error
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object 
+ */
+ router.route("/addUser").post(postUser);
+
+/**
+ * @swagger
+ * /users:
+ *   post:
+ *     summary: get Users
+ *     tags: [User]
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
  *                 type: string
  *     responses:
  *       200:
@@ -75,9 +123,47 @@ const router = express.Router();
  *                type: object
  *
  */
+ router.route("/users").post(getUser);
 
-router.route("/pushCommand").post(postUser);
-
-router.route("/commands").get(getUser);
+/**
+ * @swagger
+ * /delete:
+ *   delete:
+ *     summary: delete User
+ *     tags: [User]
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: ok
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *       404:
+ *         description: not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *       500:
+ *         description: error
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *
+ */
 router.route("/delete").delete(deleteUser);
+
+
+ 
+
+
 export default router;
