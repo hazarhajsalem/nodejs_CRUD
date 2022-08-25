c1369c341f1848a00913b297fcf196336c794d6d
hazarhajsalem <hazarhaj2000@gmail.com>
2022-08-25 16:52:33 +0100

    feat: add `getBook`  `deleteLivre` `getBookwhere`  `editLivre` method in `commandeRouter` component

--- src/router/CommandeRouter.ts
+++ src/router/CommandeRouter.ts
@@ -3,7 +3,10 @@ import { addLivre } from "../controllers/addLivre";
 import { postUser } from "../controllers/postUser";
 import { getUser } from "../controllers/getUser";
 import { deleteUser } from "../controllers/deleteUser";
-
+import {getBook} from "../controllers/getBook";
+import { deleteLivre } from "../controllers/deleteLivre";
+import {getBookwhere} from "../controllers/getBook";
+import {editLivre} from "../controllers/editLivre";
 const router = express.Router();
 
 /**
@@ -163,7 +166,153 @@ const router = express.Router();
 router.route("/delete").delete(deleteUser);
 
 
- 
+/**
+ * @swagger
+ * /get:
+ *   get:
+ *     summary: get Livre
+ *     tags: [Livre]
+ *     responses:
+ *       200:
+ *         description: ok
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *       404:
+ *         description: not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *       500:
+ *         description: error
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *
+ */
+  router.route("/get").get(getBook);
+/**
+ * @swagger
+ * /deleteLivre:
+ *   delete:
+ *     summary: delete Livre
+ *     tags: [Livre]
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               isbn:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: ok
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *       404:
+ *         description: not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *       500:
+ *         description: error
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *
+ */
+ router.route("/deleteLivre").delete(deleteLivre);
+
+
+/**
+ * @swagger
+ * /getLivre:
+ *   post:
+ *     summary: get Livre
+ *     tags: [Livre]
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               titre:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: ok
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *       404:
+ *         description: not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *       500:
+ *         description: error
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *
+ */
+ router.route("/getLivre").post(getBookwhere);
+
+
+
+ /**
+ * @swagger
+ * /Update:
+ *   post:
+ *     summary: Update Livre
+ *     tags: [Livre]
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               isbn:
+ *                 type: string
+ *               titre:
+ *                 type: string
+ *               auteur:
+ *                 type: string
+ *               categorie:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: ok
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *       404:
+ *         description: not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *       500:
+ *         description: error
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object 
+ */
+  router.route("/Update").post(editLivre);
+
 
 
 export default router;
